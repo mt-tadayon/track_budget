96d7d1ecac4164e9f7724ef5a3929a78648e3cb3
Myracle <max.weber.consultant@outlook.com>
2018-11-25 13:00:29 +1100

    The server is now runnable

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,32 +1,61 @@
-import Hapi from "hapi";
+import { ApolloServer, gql } from 'apollo-server-hapi';
+import * as Hapi from 'hapi';
 
-// Create a server with a host and port
-const server = new Hapi.Server({
-    host: 'localhost',
-    port: 8000
-});
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    },
+];
 
-// Add the route
-server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, h) {
-        return 'hello world';
-    }
-});
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
 
-// Start the server
-async function start() {
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
 
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
 
-    console.log('Server running at:', server.info.uri);
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
 };
 
-start();
+async function startServer() {
+    const server = new ApolloServer({ typeDefs, resolvers })
+
+    const app = new Hapi.Server({
+        host: 'localhost',
+        port: 4000
+    })
+
+    await server.applyMiddleware({
+        app
+    })
+
+    await server.installSubscriptionHandlers(app.listener);
+
+    await app.start().then(() => console.log(`Server is started on app.info.uri`));
+}
+
+startServer().catch(error => console.log(error));
