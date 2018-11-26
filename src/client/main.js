8239bdc686a8a79cd5f18c2ad0bc4f927923de56
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-11-26 11:58:47 +1100

    feat: create a table and input  field to enter feed

--- src/client/main.js
+++ src/client/main.js
@@ -1,17 +1,32 @@
-const myNames = ["Mahtab", "Max", "Marjan", "Isaac"]
+document.getElementById("confirm").addEventListener("click", displayData);
 
-function listOfRequiredNames(arrayNames) {
-    const ul = document.createElement("ul");
 
-    for (let i = 0; i < arrayNames.length; i++) {
-        const listItem = document.createElement("li");
-        const textOfItem = document.createTextNode(arrayNames[i]);
-        listItem.appendChild(textOfItem);
-        ul.appendChild(listItem);
+function listOfData(arrayData) {
+    const row = document.createElement("tr");
+
+    for (let i = 0; i < arrayData.length; i++) {
+        const cell = document.createElement("td");
+        const textOfCell = document.createTextNode(arrayData[i]);
+        cell.appendChild(textOfCell);
+        row.appendChild(cell);
     }
-    document.body.appendChild(ul);
+    return row;
 }
 
-function writeNewName() {
-    const enterNewName = document.getElementById("name").value;
+function displayData() {
+    function myData() {
+        let arrayInput = [];
+
+        for (let i = 0; i < $("input").length; i++) {
+            arrayInput.push($("input")[i].value);
+        }
+
+        let selectcurrancy = $("#currancy-select").val();
+        arrayInput.push(selectcurrancy);
+        return arrayInput;
+    }
+
+    let mainTable = document.getElementById("mainTable");
+    let data = myData();
+    mainTable.appendChild(listOfData(data));
 }
