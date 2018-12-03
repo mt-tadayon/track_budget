6741df7f4c0529322e1c9038cb96243393b7b006
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-12-04 12:59:28 +1300

    feat: create a basic css

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('Budget Tracker');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Budget Tracker!');
   });
 });
