1f709822d81a5e6c5eb5bad242e3e16ba58a84ec
Mahtab Tadayon <mahtab.tadayon@web.de>
2018-12-04 14:38:02 +1300

    feat: instal bootstrap

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
